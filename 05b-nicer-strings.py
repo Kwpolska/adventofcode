cb3474deb6067eb85c4a77f422c87595b1fe74c1
Chris Warrick <kwpolska@gmail.com>
2015-12-06 20:30:48 +0100

    Fix Day 05 (thanks /u/C0urante)
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- 05b-nicer-strings.py
+++ 05b-nicer-strings.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python
 import string
 import re
-import time
 NICE = 0
 R = []
 for l in string.ascii_lowercase:
     R.append(re.compile("{0}\w{0}".format(l)))
 
 def test(s):
-    #print(s)
-    # 1a. Contains a pair that appears at least twice
-    pairs = [''.join(i) for i in zip(s, s[1:])]
-    seen = []
-    f1a = False
-    f1l = []
-    for p in pairs:
-        if p in seen:
-            f1a = True
-            f1l.append(p)
-        else:
-            seen.append(p)
-    if not f1a:
-        #print("- 1a")
+    print(s)
+    # 1. Contains a pair that appears at least twice, but cannot overlap
+    f1 = False
+    # "borrowed" from /u/C0urante: https://www.reddit.com/r/adventofcode/comments/3viazx/day_5_solutions/cxnswjz
+    # (previous solution used zip(), which was apparently wrong)
+    for i in range(len(s) - 3):
+        pair = s[i:i + 2]
+        next = s[i + 2:]
+        if pair in next:
+            f1 = True
+    if not f1:
         return False
-    else:
-        print(s)
-        print("+ 1a {0}".format(f1l))
-    # 1b. But cannot overlap
-    for i in f1l:
-        flag = 0
-        for n, c in enumerate(s):
-            if flag == 0 and c == i[0]:
-                flag += 1
-            elif flag == 1 and c == i[1]:
-                flag = 0
-                # found a pair. now let’s test others.
-                try:
-                    if s[n] + s[n + 1] in f1l:
-                        print("- 1b")
-                        return False
-                except IndexError:
-                    pass
-    print("+ 1b")
     # 2. One letter which repeats with exactly one letter in between
     for r in R:
         m = r.search(s)
