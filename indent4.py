2d37bc0f3732e69369929b895f97af407d4a0df8
Chris Warrick <kwpolska@gmail.com>
2015-12-09 17:59:57 +0100

    Add indent4.py (for sweet reddit karma)
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

---
+++ indent4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import textwrap
+import sys
+if len(sys.argv) != 2:
+    print("usage: ./indent4.py FILENAME")
+    exit(2)
+else:
+    with open(sys.argv[1]) as fh:
+        print(textwrap.indent(fh.read(), 4 * ' '))
