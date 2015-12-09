bf93aba12b4a60e45ba7eeb8dda37ade6b544849
Chris Warrick <kwpolska@gmail.com>
2015-12-09 07:58:50 +0000

    Day 09
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

---
+++ 09-distances.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import itertools
+places = set()
+pairs = {}
+with open('09-input.txt') as fh:
+    for l in fh:
+        place1, _, place2, _, distance = l.strip().split()
+        distance = int(distance)
+        places.add(place1)
+        places.add(place2)
+        pairs[(place1, place2)] = distance
+        pairs[(place2, place1)] = distance
+places = list(places)
+# get rid of two places
+P = itertools.permutations(places)
+ROUTES = {}
+for i in P:
+    distance = 0
+    # Check each entry separately.
+    for n in range(len(i) - 1):
+        try:
+            distance += pairs[(i[n], i[n + 1])]
+        except KeyError:
+            distance = -1
+            break
+    if distance != -1:
+        ROUTES[i] = distance
+print(min(ROUTES.values()))
+print(max(ROUTES.values()))
