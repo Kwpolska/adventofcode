2b84c29ddfad47cf38ba44317cc041c5c0ebf637
Chris Warrick <kwpolska@gmail.com>
2016-12-02 17:36:26 +0100

    Add a template for future challenges

---
+++ 2016/template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+with open("input/💾.txt") as fh:
+    file_data = fh.read()
+
+
+def solve(data):
+    return data
+
+
+test_data = "📄"
+test_output = solve(test_data)
+test_expected = "🏁"
+assert test_output == test_expected
+print(solve(file_data))
