c33ea386bb5264312bafe9e6be529b9453ca5137
Chris Warrick <kwpolska@gmail.com>
2017-12-06 20:50:56 +0100

    Day 6
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

---
+++ 2017/day06b_and_now_we_need_a_dict.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+def solve(data):
+    state = [int(i) for i in data.split()]
+    seen = {}
+    steps = 0
+    while tuple(state) not in seen:
+        seen[tuple(state)] = steps
+        # Pick largest index.
+        to_redist = max(state)
+        i = state.index(to_redist)  # first (avoids collisions)
+        state[i] = 0
+        i += 1
+        while to_redist:
+            if i >= len(state):
+                i = 0
+            state[i] += 1
+            to_redist -= 1
+            i += 1
+        steps += 1
+    return steps - seen[tuple(state)]
+
+
+test_data = "0 2 7 0"
+test_output = solve(test_data)
+test_expected = 4
+print(test_output, test_expected)
+assert test_output == test_expected
+print(solve("0 5 10 0 11 14 13 4 11 8 8 7 1 4 12 11"))
